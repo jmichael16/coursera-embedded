45b13d0c88d5476644434c6522342bec62731d73
Jake Michael <james.michael@colorado.edu>
2020-04-09 14:42:36 -0600

    added functioning code and documentation

--- stats.h
+++ stats.h
@@ -19,20 +19,88 @@
 
 #ifndef __STATS_H__
 #define __STATS_H__
-
-/* Add Your Declarations and Function Comments here */ 
+#include <stdint.h>
+/**
+ * @brief - prints statistics
+ *
+ * prints minimum, maximum, mean and median
+ *
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return - none
+ */
+void print_statistics(uint8_t *arr, uint8_t len);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief - prints a character array
+ * 
+ * prints a character array in increasing order by index
  *
- * <Add Extended Description Here>
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return - none
  */
+void print_array(uint8_t *arr, uint8_t len);
+
+/**
+ * @brief - finds the median of a char array
+ *
+ * finds median, assumes all positive 8 bit integers
+ * in the input array 
+ *
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return - the median rounded down to nearest integer
+ */
+uint8_t find_median(uint8_t *arr, uint8_t len);
+
+/**
+ * @brief - finds the mean of a char array
+ *
+ * finds mean and rounds down to nearest integer
+ *
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return the median
+ */
+uint8_t find_mean(uint8_t *arr, uint8_t len);
+
+/**
+ * @brief - finds the maximum of a char array
+ * 
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return - the maximum
+ */
+uint8_t find_maximum(uint8_t *arr, uint8_t len);
+
+/**
+ * @brief - finds the minimum of a char array
+ *
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return - the minimum 
+ */
+uint8_t find_minimum(uint8_t *arr, uint8_t len);
+
+/**
+ * @brief - sorts array in descending order
+ *
+ * sorts the array in descending order 
+ * (largest to smallest) 
+ *
+ * @param arr - a pointer to the character array
+ * @param len - the length of the array
+ *
+ * @return - a pointer to the sorted character array 
+ */
+uint8_t* sort_array(uint8_t *arr, uint8_t len);
+
 
 #endif // __STATS_H__ 
